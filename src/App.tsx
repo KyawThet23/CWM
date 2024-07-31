a36ea23deae065f8ce4e63d5ec9079d73330dd6c
kyawThetHtun <kyawthe235@gmail.com>
2024-07-31 23:09:51 +0630

    pass children via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import ListGroup from "./components/listGroup";
+// import Alert from "./components/alert";
+import { useState } from "react";
+import Button from "./components/button";
+import Alert from "./components/alert";
+// import ListGroup from "./components/listGroup";
 
 function App() {
-  const items = ["Mandalay", "Yangon", "Taunggyi", "Thibaw", "Naung cho"];
+
+  const [alertVisible,setAlertVisible] = useState(false);
+  
+  // const items = ["Mandalay", "Yangon", "Taunggyi", "Thibaw", "Naung cho"];
+  // const handleSelectItem = (item : string) => {
+  //   console.log(item)
+  // }
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      {/* <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/> */}
+
+      {/* <Alert>
+        Hello <span>Alert</span> 
+      </Alert> */}
+
+      { alertVisible && <Alert onClose={() => setAlertVisible(false)}>Alert is showing.</Alert> }
+      <Button children="My button" onClick={() => setAlertVisible(true)} color="secondary"/>
+
     </div>
   );
 }
--- src/components/listGroup.tsx
+++ src/components/listGroup.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-interface Props{
+interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({items , heading} : Props) {
-
+function ListGroup({ items, heading, onSelectItem }: Props) {
   // Hook
-  const [selectedIndex,setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const getMessage = () => {
     return items.length == 0 && <p>No items found</p>;
@@ -19,10 +19,17 @@ function ListGroup({items , heading} : Props) {
       <h1>{heading}</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item , index) => (
+        {items.map((item, index) => (
           <li
-            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
-            onClick={() => setSelectedIndex(index)}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
             key={item}
           >
             {item}
