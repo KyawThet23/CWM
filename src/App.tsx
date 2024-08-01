b4a9e6c3eb1218d8e5d9c08a04ebb8c48c8307a1
kyawThetHtun <kyawthe235@gmail.com>
2024-08-01 22:14:08 +0630

    react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@
 // import { useState } from "react";
 // import Button from "./components/button";
 // import Alert from "./components/alert";
-import ListGroup from "./components/listGroup";
+// import ListGroup from "./components/listGroup";
+
+import Like from "./components/like";
 
 function App() {
 
   // const [alertVisible,setAlertVisible] = useState(false);
   
-  const items = ["Mandalay", "Yangon", "Taunggyi", "Thibaw", "Naung cho"];
-  const handleSelectItem = (item : string) => {
-    console.log(item)
-  }
+  // const items = ["Mandalay", "Yangon", "Taunggyi", "Thibaw", "Naung cho"];
+  // const handleSelectItem = (item : string) => {
+  //   console.log(item)
+  // }
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      {/* <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/> */}
 
       {/* <Alert>
         Hello <span>Alert</span> 
@@ -23,6 +25,8 @@ function App() {
 
       {/* { alertVisible && <Alert onClose={() => setAlertVisible(false)}>Alert is showing.</Alert> }
       <Button children="My button" onClick={() => setAlertVisible(true)} color="secondary"/> */}
+
+      <Like onClick={() => console.log('clicked')}/>
 
     </div>
   );
