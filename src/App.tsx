2105a6d05cc223d3e0d643367c8ab7c62c829e52
kyawThetHtun <kyawthe235@gmail.com>
2024-08-02 22:33:25 +0630

    update state

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,20 @@
 // import Alert from "./components/alert";
 // import ListGroup from "./components/listGroup";
 
-import Like from "./components/like";
+import { useState } from "react";
+
+// import Like from "./components/like";
 
 function App() {
+
+  const [pizza,setPizza] = useState({
+    name: 'Spicy Pepperoni',
+    toppings: ['Mushroom']
+  });
+
+  const handleAddToping = () => {
+    setPizza({ ...pizza, toppings: [...pizza.toppings , 'Cheese'] })
+  }
 
   // const [alertVisible,setAlertVisible] = useState(false);
   
@@ -26,7 +37,11 @@ function App() {
       {/* { alertVisible && <Alert onClose={() => setAlertVisible(false)}>Alert is showing.</Alert> }
       <Button children="My button" onClick={() => setAlertVisible(true)} color="secondary"/> */}
 
-      <Like onClick={() => console.log('clicked')}/>
+      {/* <Like onClick={() => console.log('clicked')}/> */}
+
+      <button onClick={handleAddToping}>
+        Add
+      </button>
 
     </div>
   );
